796943cdd8f5f2c943a9f1468dfe72f63174f74a
Gareth Paul Jones <gjones@twitter.com>
2014-12-14 23:26:45 -0800

    adding changes and some commentary to the REST API request

--- settee/TVSearchAPI.swift
+++ settee/TVSearchAPI.swift
@@ -12,43 +12,63 @@ import TwitterKit
 
 func Search(completion: (result: [String]) -> Void) {
 
+    // setup some type aliases to handle regular wording for JSON type objects
     typealias JSON = AnyObject
     typealias JSONDictionary = Dictionary<String, JSON>
     typealias JSONArray = Array<JSON>
 
+    // set an endpoint you can check out the docs via:
+    // https://dev.twitter.com/rest/reference/get/search/tweets
     let statusesShowEndpoint = "https://api.twitter.com/1.1/search/tweets.json"
+
+    // setup the params for the request
     let params = ["q": "#NETFLIX OR #BBCOne", "count": "50"]
+
+    // setup container for an error
     var clientError : NSError?
+
+    // Initialize Twitter
     Twitter.initialize()
 
+    // Send a REQUEST to Twitter using GuestAuthentication e.g. no authenticated user just the app.
     Twitter.sharedInstance().logInGuestWithCompletion{
         (session, error) -> Void in
         if (session != nil) {
-            /// go
+
+            // woohoo we have a session - let's get crazy
             let request = Twitter.sharedInstance().APIClient.URLRequestWithMethod("GET", URL:  statusesShowEndpoint, parameters: params, error:&clientError)
 
+            // if the request is ready to rock and roll
             if request != nil {
 
-
+                // let's send us a REST API reuest
                 Twitter.sharedInstance().APIClient.sendTwitterRequest(request) {
                     (response, data, connectionError) -> Void in
                     if (connectionError == nil) {
+
+                        // Setup a tweet array to contain all of those juicy tweets
                         var tweetArray = Array<String>()
+
                         var jsonError : NSError?
                         let json : AnyObject? =
                         NSJSONSerialization.JSONObjectWithData(data,
                             options: nil,
                             error: &jsonError)
 
-                        println(json)
-
+                        // Iterate through JSON response and append the values to the TweetArray
                         if let statuses = json!["statuses"] as? JSONArray {
+
+                            // For each tweet in the status block of the json request e.g. {"statuses": [tweets.........
                             for tweet in statuses {
                                 if let id = tweet["id_str"] as?String{
+
+                                    // Append the Tweet to the array
                                     tweetArray.append(id)
                                 }
                             }
                         }
+
+                        // complete this magical request
                         completion(result: tweetArray)
                     }
 
